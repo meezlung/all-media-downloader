83a82484d8aa017b4ef401b3dec1c7977b5a54a7
Gabriel Mislang <gabrielmislang101@gmail.com>
2023-09-05 18:06:10 +0800

    Add all-media downloader API

--- reel-downloader.py
+++ reel-downloader.py
@@ -1,5 +1,7 @@
 from pytube import YouTube
 
+import requests
+
 class Youtube():
     def convert(self, url, format, resolution):
         try:
@@ -20,3 +22,21 @@ class Youtube():
 # Create an instance of ReelDownloader
 downloader = Youtube()
 # downloader.youtube_convert("https://www.youtube.com/shorts/5WqLjXTuvyU", "mp4", "720p")
+
+
+class Media():
+    def convert(video_url):
+        url = "https://all-media-downloader.p.rapidapi.com/rapid_download/download"
+
+        payload = { "url": f"{video_url}" }
+        headers = {
+            "content-type": "application/x-www-form-urlencoded",
+            "X-RapidAPI-Key": "d841669306mshbe4832b0a2b908fp1cb62ejsn48da473bd1df",
+            "X-RapidAPI-Host": "all-media-downloader.p.rapidapi.com"
+        }
+
+        response = requests.post(url, data=payload, headers=headers)
+
+        print(response.json())
+
+Media.convert("https://www.instagram.com/p/CwgGF6rrrDg/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==")
